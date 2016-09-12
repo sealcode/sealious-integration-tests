77ac9735d44fb90ad96db9cbd4dbce9edfcd1db7
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-09-12 12:39:18 +0200

    Create sperate dir for test methods

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var pre = require("./pre-test.js");
-var test = require("./test.js");
+var post = require("./methods/post.js");
 
 
 pre()
-.then(test)
+.then(post)
 .then(()=> process.exit())
 .catch((err) => {console.error(err); process.exit(1)});
