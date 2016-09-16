eaa5f12610bab0faa53a4a04aa1489c0f6068e68
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-09-16 15:58:21 +0200

    Test basic functionality sealious.yml

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var assert = require("assert");
 
 function uri(path){
-	return "http://localhost:8080/api/v1/" + path;
+	return "http://localhost:8081/api/v1/" + path;
 }
 
 module.exports = function(){
