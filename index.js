f5102d1cd66033dbdac5d6b915f3f3f6a702117f
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-09-16 17:33:26 +0200

    Run all tests at once

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+"use strict";
+const Promise = require("bluebird");
 var pre = require("./pre-test.js");
 var methods = {
 	add: require("./methods/add.js"),
 	patch: require("./methods/patch.js"),
 	put: require("./methods/put.js"),
-	delete: require("./methods/delete.js")
+	delete: require("./methods/delete.js"),
 }
 
 pre()
-.then(methods[process.argv[2]])
-.then(()=> process.exit())
-.catch((err) => {console.error(err); process.exit(1)});
+.then(function(){
+	return Promise.each(
+		Object.keys(methods), 
+		(key)=>{
+			console.log("Running scenario:", key);
+			return methods[key]()
+			.then(function(){
+				console.log("\t...success!");
+			});
+		});
+}).then(()=> {
+	console.log("Tests run complete. Exiting with status 0.");
+	process.exit(0);
+})
+.catch((err) => {
+	console.error(err); 
+	console.log(err.stack);
+	process.exit(1);
+});
