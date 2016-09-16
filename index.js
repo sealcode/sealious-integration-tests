5c85c70d694e9eead052a0643586c7fe2e95b62b
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-09-16 15:34:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var pre = require("./pre-test.js");
-var test = require("./test.js");
-
+var methods = {
+	add: require("./methods/post.js"),
+	patch: require("./methods/patch.js"),
+	put: require("./methods/put.js"),
+	delete: require("./methods/delete.js")
+}
 
 pre()
-.then(test)
+.then(methods[process.argv[2]])
 .then(()=> process.exit())
 .catch((err) => {console.error(err); process.exit(1)});
