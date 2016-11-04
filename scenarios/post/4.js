384843269208f1dd6342308b2468765c5bac34ce
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-11-04 22:25:26 +0100

    Add new test - Post new element without some fields (check non-existent fields)

---
+++ scenarios/post/4.js
@@ -0,0 +1,25 @@
+// scenario #4
+// TODO: Post new element without some fields (check non-existent fields)
+
+"use strict";
+var rp = require("request-promise");
+var fs = require("fs");
+var assert = require("assert");
+
+var uri = (path) => "http://localhost:8081/api/v1/" + path;
+
+module.exports = function() {
+	var prepared_body = {
+		name: "Some title"
+	}
+	return rp.post({
+		url: uri('collections/people'),
+		formData: prepared_body,
+		json: true,
+		resolveWithFullResponse: true
+	})
+	.then((res) => {
+		if (res.body.age !== undefined) throw new Error(`the body contains fields with 'undefined' value, which were not added`)
+		else console.log("success!")
+	})
+};
