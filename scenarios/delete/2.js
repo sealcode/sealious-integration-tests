d7d1d81d17d1325d4ebaf065d3c51570d34a3108
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-11-07 14:04:17 +0100

    Add new test - Delete non-existing element from collection

---
+++ scenarios/delete/2.js
@@ -0,0 +1,25 @@
+// scenario #2
+// TODO: Delete non-existing element from collection
+
+"use strict";
+var rp = require("request-promise");
+var fs = require("fs");
+var assert = require("assert");
+
+var uri = (path) => "http://localhost:8081/api/v1/" + path;
+
+module.exports = function() {
+	var id = Math.random()
+	return rp.delete({
+		url: uri(`collections/people/${id}`),
+		json: true,
+		resolveWithFullResponse: true
+	})
+	.catch((res) => {
+		if (res.statusCode !== 404) {
+			throw new Error('incorrect status code, received ' + res.statusCode)
+		} else {
+			console.log("success!")
+		}
+	})
+};
