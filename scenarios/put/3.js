e70a88e82cdf9078089234d1813327bacb7e5a60
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-11-10 22:48:04 +0100

    Add new test - Update element of collection by PUT method without required field

---
+++ scenarios/put/3.js
@@ -0,0 +1,41 @@
+// scenario #3
+// TODO: Update element of collection by PUT method without required field
+
+"use strict";
+var rp = require("request-promise");
+var fs = require("fs");
+var assert = require("assert");
+var clc = require('cli-color');
+var uri = (path) => "http://localhost:8081/api/v1/" + path;
+
+module.exports = function() {
+	var initial_body = {name: "Title-1", age: 21}
+	var updated_body = {age: 23}
+
+	return rp.post({
+		url: uri('collections/people'),
+		formData: initial_body,
+		json: true,
+		resolveWithFullResponse: true
+	})
+	.then((res) => {
+		if (res.statusCode !== 201) {
+			throw new Error(clc.red('incorrect status code, received ' + res.statusCode))
+		} else {
+			var id = res.body.id;
+			return rp.put({
+				url: uri(`collections/people/${id}`),
+				formData: updated_body,
+				json: true,
+				resolveWithFullResponse: true
+			})
+		}
+	})
+	.then((res) => {
+		throw new Error(clc.red("Should have thrown a error which is related to incorrect status code"))
+	})
+	.catch((res) => {
+		if (res.statusCode !== 403) throw new Error(clc.red('incorrect status code, received ' + res.statusCode))
+	})
+	.then(() => console.log("succcess!"));
+};
